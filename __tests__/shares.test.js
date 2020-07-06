7a43bafd2a74021e30b1c16ece52d020ff089b92
keegers <42276368+keegers@users.noreply.github.com>
2020-07-06 14:37:28 -0700

    add share model

---
+++ __tests__/shares.test.js
@@ -0,0 +1,23 @@
+const { agent,  prepare, getLoggedInUser } = require('../db/data-helpers');
+const request = require('supertest');
+const app = require('../lib/app');
+const Share = require('../lib/models/Share');
+
+
+describe('Share routes', () => {
+    it('Creates share with POST', async() => {
+
+    });
+    
+    it('Retrieves share with GET', async() => {
+
+    });
+    
+    it('Updates share with PATCH', async() => {
+
+    });
+    
+    it('Delete a share with DELETE', async() => {
+        
+    });
+});
